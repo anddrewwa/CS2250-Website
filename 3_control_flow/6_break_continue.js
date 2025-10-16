cfea2e2f7fe7a363b17cf53b56cbe5b61e34a3d8
Andrew Chen <161515277+anddrewwa@users.noreply.github.com>
2025-10-16 22:45:32 +0000

    add

--- 3_control_flow/6_break_continue.js
+++ 3_control_flow/6_break_continue.js
@@ -1,9 +1,20 @@
 const numbers = [12,13,14,15];
-
+/**
+ * 
+ * index = 3
+ * numbers[3] = 15;
+ * 
+ * 
+ */
 for( let i = 0; i < numbers.length; i++){
-    if(numbers[i] >= 15){
-        break;
+    if(numbers[i] === 13){
+        continue;
     }
     console.log(numbers[i]);
-    
 }
+
+
+
+
+
+
